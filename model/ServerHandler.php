043e783821edb06042a8511325d45329a41d7406
Jan-Frederik Rieckers <rieckers@uni-bremen.de>
2020-07-25 16:02:16 +0200

    fixup! Fix error in serverHandler

--- model/ServerHandler.php
+++ model/ServerHandler.php
@@ -25,7 +25,7 @@ class ServerHandler extends PFAHandler {
            'created'        => pacol(   0,        0, 0, 'ts',   'created',       ''),
            'modified'       => pacol(   0,        0, 0, 'ts',   'last_modified', ''),
            '_can_edit'      => pacol(   0,        0, 1, 'int',  '',              '', 0, array(), 0, 1, (Config::bool('multiple_servers') && $this->is_superadmin) . 'as _can_edit' ),
-           '_can_delete'    => pacol(   0,        0, 1, 'int',  '',              '', 0, array*(, 0, 1, (Config::bool('multiple_servers') && $this->is_superadmin) . 'as _can_delete' ),
+           '_can_delete'    => pacol(   0,        0, 1, 'int',  '',              '', 0, array(), 0, 1, (Config::bool('multiple_servers') && $this->is_superadmin) . 'as _can_delete' ),
         );
     }
 
